74abc706c2058be671c0314cda86856f43bbd326
Kovari Armand <kovariarmand@icloud.com>
2017-04-05 13:14:18 +0200

    save rating

---
+++ PonteMovie/UserDefaultsRatingExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UserDefaultsRatingExtension.swift
+//  PonteMovie
+//
+//  Created by Kovari Armand on 2017. 04. 05..
+//  Copyright © 2017. Kovari Armand. All rights reserved.
+//
+
+import UIKit
+
+class UserDefaultsRatingExtension: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
